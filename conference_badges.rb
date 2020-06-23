dcbad6327876ea23c28c428329fc2b3a5f1a2042
MUHANNAD JABER <abujaber44@gmail.com>
2020-06-23 20:52:03 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,6 +5,6 @@ end
 def batch_badge_creator(name)
   names = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 names.each do |name|
-  return ("Hello, my name is #{name}.")
+  puts ("Hello, my name is #{name}.")
  end
 end 
